2d2f286ed1fa8cab6aa44a4a3d183df4a10228de
jchizz <jic217@lehigh.edu>
2015-01-25 22:54:46 +0000

    Finished Homework 1, WelcomeClass message

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Jonathan Chisholm
+// 1/27/2015
+// Homework 1
+// Welcome Class Program
+
+// first compile the program javac WelcomeClass.java
+
+// then, run the program java WelcomeClass
+
+// define the class
+public class WelcomeClass {
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        // print the welcome message
+        // blackslashes are an escape key in java, you need two next to each other to nullify this
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-J--I--C--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I am a sophomore at Lehigh University. I major in Finance and play Ultimate Frisbee.");
+    
+    }
+}
